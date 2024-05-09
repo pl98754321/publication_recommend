3bfee7a7ee984bca720e6830041ad43a1faa32bf
Supharoek Kongprapane <pream5138@gmail.com>
2024-05-09 17:21:38 +0700

    add docker compose

--- fastapi/app/services/__init__.py
+++ fastapi/app/services/__init__.py
@@ -2,6 +2,6 @@ from .dataloader import *
 from .dataloader_np import *
 from .logger import get_logger
 
-pub_dataloader = PubDataLoader(r"./data_process/pub.csv")
-ref_dataloader = RefDataLoader(r"./data_process/ref.csv")
+pub_dataloader = PubDataLoader(r"./data_process/pub_preprocessed.csv")
+ref_dataloader = RefDataLoader(r"./data_process/ref_preprocessed.csv")
 aff_dataloader = AffilDataLoader(r"./data_process/affiliation.csv")
